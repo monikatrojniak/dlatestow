17d547c5dd2c007fa34d1c58b6f6ad675ba1a775
Monika <monika@trojniak.net>
2016-03-02 23:14:36 +0100

    Dodanie petli

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ php_info();
 $zmienna = 'testowa zmienna';
 
 echo $zmienna;
+$tab = array(1,25,45,214,789);
 
 $x = 1;
 $y = 2;
@@ -16,3 +17,7 @@ echo $z;
 
 echo $x-$y;
 
+foreach($tab as $zmienna)
+{
+	
+}
